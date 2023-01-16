75b29b846a24425813715903ba1be9e6545537d1
unknown <coke.jgomez@gmail.com>
2023-01-16 20:47:52 -0300

    Fix de respuesta del HTTPStatus en el controlador de SHOW By Id

--- src/main/java/cl/cokke/controllers/ClienteRestController.java
+++ src/main/java/cl/cokke/controllers/ClienteRestController.java
@@ -45,7 +45,7 @@ public class ClienteRestController {
 		}catch(DataAccessException e) {
 			response.put("mensaje", "Error en la base de datos");
 			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
-			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
 		if(cliente == null) {
