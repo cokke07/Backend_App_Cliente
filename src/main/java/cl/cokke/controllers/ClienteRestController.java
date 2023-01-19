6230024420c81fe174ca8fb79a91a85d2c405f83
unknown <coke.jgomez@gmail.com>
2023-01-19 19:37:51 -0300

    Se agrega el valid y el bindigResult para que responda un listado de errores en caso de fallar

--- src/main/java/cl/cokke/controllers/ClienteRestController.java
+++ src/main/java/cl/cokke/controllers/ClienteRestController.java
@@ -3,11 +3,15 @@ package cl.cokke.controllers;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -57,10 +61,20 @@ public class ClienteRestController {
 	}
 
 	@PostMapping("clientes/create")
-	public ResponseEntity<?> create(@RequestBody Cliente cliente) {
+	public ResponseEntity<?> create(@Valid @RequestBody Cliente cliente, BindingResult result) {
 
 		Cliente clienteNuevo = null;
 		Map<String, Object> response = new HashMap<>();
+		
+		if(result.hasErrors()) {
+			List<String> errors = result.getFieldErrors()
+					.stream()
+					.map(err -> "El campo '" + err.getField() +"' "+ err.getDefaultMessage())
+					.collect(Collectors.toList());
+			response.put("errors", errors);
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
+		
 		try {
 			clienteNuevo = clienteService.save(cliente);
 
